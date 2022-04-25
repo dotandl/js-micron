ee2df0f7c088866ad1a1a0256ff7381778dc61be
dotandl <oskarzawidlak@gmail.com>
2022-04-25 13:22:14 +0200

    Add simple CLI interface

--- src/compress.js
+++ src/compress.js
@@ -2,6 +2,9 @@ const UglifyJS = require('uglify-js');
 const Compressor = require('./Compressor');
 
 module.exports = function (code) {
+  if (process.env.VERBOSE) {
+    console.log('Optimizing code using UglifyJS & preparing for analyze...');
+  }
   // Compress the code, shorten identifiers and beautify (to make it easier for Compressor to analyze)
   code = UglifyJS.minify(code, {
     mangle: true,
@@ -10,9 +13,15 @@ module.exports = function (code) {
     output: { beautify: true },
   }).code;
 
+  if (process.env.VERBOSE) {
+    console.log('Analyzing code...');
+  }
   const compressor = new Compressor();
   compressor.analyze(code);
 
+  if (process.env.VERBOSE) {
+    console.log('Minifying code using UglifyJS...');
+  }
   // Again compress the code, but this time without beautifying
   code = UglifyJS.minify(code, {
     mangle: true,
@@ -20,6 +29,9 @@ module.exports = function (code) {
     toplevel: true,
   }).code;
 
+  if (process.env.VERBOSE) {
+    console.log('Compressing code...');
+  }
   code = compressor.compress(code);
 
   return code;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,59 @@
 #!/usr/bin/env node
+
+const path = require('path');
+const fs = require('fs');
+const GetOpt = require('node-getopt');
+const compress = require('./compress');
+
+function main() {
+  console.log('== js-micron ==');
+  console.log('Input file:', inputFile);
+  console.log('Output file:', outputFile);
+
+  if (process.env.VERBOSE) {
+    console.log(`Reading code from ${inputFile}...`);
+  }
+  const js = fs.readFileSync(inputFile, 'utf8');
+  const compressedJS = compress(js);
+
+  if (process.env.VERBOSE) {
+    console.log(`Writing code to ${outputFile}...`);
+  }
+  fs.writeFileSync(outputFile, compressedJS, { encoding: 'utf8' });
+
+  // TODO: compression summary
+  console.log('Done.');
+}
+
+const opt = new GetOpt([
+  ['h', 'help', 'display this help'],
+  ['V', 'version', 'display version'],
+  ['v', 'verbose', 'verbose output'],
+]);
+
+opt.setHelp(
+  `Usage: node ${path.basename(
+    __filename
+  )} [OPTIONS] <INPUT FILE> <OUTPUT FILE>\n\nOptions:\n[[OPTIONS]]\n`
+);
+
+opt.bindHelp();
+opt.parseSystem();
+
+const { options, argv } = opt;
+const [inputFile, outputFile] = argv;
+
+if (options.version) {
+  const { version } = require(path.join(__dirname, '../package.json'));
+  console.log(`js-micron v${version}`);
+  process.exit(0);
+}
+
+if (!inputFile || !outputFile) {
+  console.error('Missing input and/or output file.');
+  opt.showHelp();
+  process.exit(1);
+}
+
+process.env.VERBOSE = options.verbose || '';
+main();
